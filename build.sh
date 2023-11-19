31ff1cd5f79e82daea04c7beb21529e61297df96
Hisan-Z <hisansunasara007@gmail.com>
2023-11-19 20:49:45 +0530

    changes

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -o errexit
 echo "BUILD START"
 pip install -r requirements.txt
 
-python3.9 manage.py collectstatic --no-input
-python3.9 manage.py migrate
-python3.9 manage.py load_products --path merge_product4.csv
+python manage.py collectstatic --no-input
+python manage.py migrate
+python manage.py load_products --path merge_product4.csv
 
 echo "BUILD END"
