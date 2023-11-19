a6a590f81ed01045a027996a73ff161904d94839
Hisan-Z <hisansunasara007@gmail.com>
2023-11-19 20:31:29 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 # exit on error
 set -o errexit
 echo "BUILD START"
-python -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
-python manage.py load_products --path merge_product4.csv
+python3.9 manage.py collectstatic --no-input
+python3.9 manage.py migrate
+python3.9 manage.py load_products --path merge_product4.csv
 
 echo "BUILD END"
